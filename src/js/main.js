29253b6c4c95d20080c38205af5f0c540b1d96b8
dubois64 <constance.dubois@studentfr.ch>
2025-05-07 16:55:30 +0200

    animation bloc 2,3,4

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+var hand = document.querySelector(".hand");
+var box1 = document.querySelector(".box1");
+
+hand.addEventListener("click", function () {
+  box1.classList.add("is-active");
+});
+
+var hand = document.querySelector(".hand");
+var box2 = document.querySelector(".box2");
+
+hand.addEventListener("click", function () {
+  box2.classList.add("is-active");
+});
+
+var hand = document.querySelector(".hand");
+var box3 = document.querySelector(".box3");
+
+hand.addEventListener("click", function () {
+  box3.classList.add("is-active");
+});
